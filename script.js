0ad45321a73ca366690be871bed55d115da4e16d
NecoBarcellosJR <154381044+NecoBarcellosJR@users.noreply.github.com>
2023-12-27 20:22:32 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function submitForm() {
+    // ... (seu código JavaScript para processar e enviar os dados)
+}
+
+function clearForm() {
+    // ... (seu código JavaScript para limpar os campos do formulário)
+}
